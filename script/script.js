57c49c3355e091bcc560adca44639579a3c0eede
Everton David <everton.097+gitkraken@gmail.com>
2022-02-14 13:11:19 -0300

    Lampada completa

--- script/script.js
+++ script/script.js
@@ -1,17 +1,24 @@
 
 var status = 1;
+var lampada = document.getElementById("lampada")
+var quarto = document.getElementById("quarto")
+var botao = document.getElementById("botao")
+
 function acionar() {
-    var lampada = document.getElementById("lampada")
-    var quarto = document.getElementById("quarto")
+
     if (status == 1) {
         status = 0;
-        lampada.setAttribute("src", "img/acesa.png")
-        quarto.style.backgroundColor = "yellow"
+        lampada.setAttribute("src", "imagens/apagada.png")
+        quarto.style.backgroundColor = "gray"
+        botao.value = "Acender"
+        document.getElementById("titulo").style.color = "gray"
     } else {
         status = 1;
         quarto.style.backgroundColor = "gray"
-        lampada.setAttribute("src", "img/apagada.png")
-
+        lampada.setAttribute("src", "imagens/acesa.png")
+        quarto.style.backgroundColor = "yellow"
+        botao.value = "Apagar"
+        document.getElementById("titulo").style.color = "yellow"
     }
 
 }
